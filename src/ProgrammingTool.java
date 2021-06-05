9835790dd27f6fbf5bbaa8593f05d0c8a225e33a
zakee-akram <Zakee-Akram@outlook.com>
2021-06-05 23:18:12 +0100

    Basic File Structure And Repo Created

---
+++ src/ProgrammingTool.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ProgrammingTool extends Application {
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/controllers/loginController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class loginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void login(ActionEvent event) throws IOException {
+        if(!passwordField.getText().isEmpty()&& !usernameField.getText().isEmpty()){
+            //Redirect To Menu Page
+            FXMLLoader loader  = new FXMLLoader(getClass().getResource("/menu.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            Stage currentStage = (Stage) usernameField.getScene().getWindow();
+            currentStage.close();
+        }
+        else{
+            System.out.println("Invalid Details");
+        }
+    }
+}
